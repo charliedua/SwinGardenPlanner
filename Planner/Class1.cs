15a6d384b215758d7a3c916ec9dde590987e353a
Anmol Dua <101983924@student.swin.edu.au>
2018-09-17 17:15:53 +1000

    Initial Commit

---
+++ Planner/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner
+{
+    /// <summary>
+    /// Replace this now
+    /// </summary>
+    public class Class1
+    {
+        public void methodOne()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PlannerTests/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Planner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Tests
+{
+    [TestClass()]
+    public class TempTests
+    {
+        [TestMethod()]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+}
