bf83f1422c04dd005fdc0db214ff37aabde7d0d1
Kieren102107262 <42164564+Kieren102107262@users.noreply.github.com>
2018-10-09 13:54:02 +1100

    Fixed the skeleton for the User class

--- Planner/User.cs
+++ Planner/User.cs
@@ -1,16 +1,21 @@
-﻿namespace Planner
+﻿using System;
+namespace Planner
 {
     /// <summary>
     /// Holds all data relating to the user, such as their budget.
     /// </summary>
     public class User
     {
-
         private float _budget;
         public float Budget
         {
             get;
             set;
+        }
+
+        public User()
+        {
+            throw new NotImplementedException();
         }
 
     }
