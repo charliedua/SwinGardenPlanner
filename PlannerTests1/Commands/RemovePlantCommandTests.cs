0e1328a9ae48f040e69f8e3448b9c5675c9d2d67
Kieren102107262 <42164564+Kieren102107262@users.noreply.github.com>
2018-10-17 10:54:02 +1100

    Finalised changes to the Remove Plant tests

--- PlannerTests1/Commands/RemovePlantCommandTests.cs
+++ PlannerTests1/Commands/RemovePlantCommandTests.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Planner.Commands;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Planner.Commands.Tests
 {
@@ -22,10 +18,10 @@ namespace Planner.Commands.Tests
             controller.Garden = new Garden(5);
             controller.Plants = new List<Plant>
             {
-                new Plant(0, "Mango") { PlantDesc = "Grows in Summers", MaxTemperature = 50f, MinTemperature = 0f, Price = 12, WaterFrequency = 12 },
-                new Plant(1, "Bananna") { PlantDesc = "Grows all time", MaxTemperature = 50f, MinTemperature = 0f, Price = 15, WaterFrequency = 0 },
-                new Plant(2,"MoneyPlant") { PlantDesc = "Grows all time", MaxTemperature = 50f, MinTemperature = 0f, Price = 500, WaterFrequency = 10 },
-                new Plant(3, "grass") { PlantDesc = "Grows all time", MaxTemperature = 50f, MinTemperature = 0f, Price = 2, WaterFrequency = 1 }
+                new Plant(0, "Mango") { PlantDesc = "Grows in Summer", MaxTemperature = 50f, MinTemperature = 0f, Price = 12, WaterFrequency = 12 },
+                new Plant(1, "Banana") { PlantDesc = "Grows any time", MaxTemperature = 50f, MinTemperature = 0f, Price = 15, WaterFrequency = 0 },
+                new Plant(2,"Money Plant") { PlantDesc = "Grows any time", MaxTemperature = 50f, MinTemperature = 0f, Price = 500, WaterFrequency = 10 },
+                new Plant(3, "Grass") { PlantDesc = "Grows any time", MaxTemperature = 50f, MinTemperature = 0f, Price = 2, WaterFrequency = 1 }
             };
         }
 
@@ -44,7 +40,7 @@ namespace Planner.Commands.Tests
             string expected = "Plant removed from garden.";
             Assert.AreEqual(expected, actual);
         }
-        [TestMethod()] // Using the wrong command syntax!
+        [TestMethod()] // Using the wrong command syntax, or spelling it wrong.
         public void TestWrongSyntax()
         {
             string expected = "Wrong Syntax.";
